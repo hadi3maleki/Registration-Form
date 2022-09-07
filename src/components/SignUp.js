fc4f400373498ef6552e9f340d20702e40d37574
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-07 12:44:33 +0430

    Show errors in span tags

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -37,26 +37,31 @@ const SignUp = () => {
                 <div>
                     <label>Name</label>
                     <input type="text" name='name' value={data.name} onChange = {changeHandler}/>
+                    {errors.name && <span>{errors.name}</span>}
                 </div>
                 
                 <div>
                     <label>Email</label>
                     <input type="text" name='email' value={data.email} onChange = {changeHandler}/>
+                    {errors.email && <span>{errors.email}</span>}
                 </div>
 
                 <div>
                     <label>Password</label>
                     <input type="password" name='password' value={data.password} onChange = {changeHandler}/>
+                    {errors.password && <span>{errors.password}</span>}
                 </div>
 
                 <div>
                     <label>Confirm Password</label>
                     <input type="password" name='confirmPassword' value={data.confirmPassword} onChange = {changeHandler}/>
+                    {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
                 </div>
 
                 <div>
                     <label>I accept terms of privacy policy</label>
                     <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange = {changeHandler}/>
+                    {errors.isAccepted && <span>{errors.isAccepted}</span>}
                 </div>
 
                 <div>
