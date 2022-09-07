1f05843da2c7138d61a94af795e04aaf24f6ba16
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-07 12:11:48 +0430

    Creating a validation.js file

---
+++ src/components/validation.js
@@ -0,0 +1,53 @@
+
+export const validation = (data)=>{
+
+    const errors = {}
+
+    //------------------------------------------
+    if(!data.name.trim()){
+        errors.name = "Username required"
+    }else{
+        delete errors.name;
+    }
+
+    //------------------------------------------
+    if(!data.email){
+        errors.email = "Email required"
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)){
+        errors.email = " Email address is invalid";
+    }else{
+        delete errors.email
+    }
+
+    //------------------------------------------
+    if(!data.password ){
+        errors.password = "Password is required";
+    }
+    else if (data.password.length < 6){
+        errors.password = "Password need to be 6 character or more";
+    }
+    else {
+        delete errors.password;
+    }
+
+    //------------------------------------------
+    if(!data.confirmPassword){
+        errors.confirmPassword ="Confirm the password";
+    }
+    else if(data.confirmPassword !== data.password){
+        errors.confirmPassword ="Password do not match";
+    }  
+    else {
+        delete errors.confirmPassword;
+    }  
+
+    //-----------------------
+    if(data.isAccepted){
+        delete errors.isAccepted
+    }
+    else {
+        errors.isAccepted = "Accept our regulations"
+    }
+
+return errors
+}
