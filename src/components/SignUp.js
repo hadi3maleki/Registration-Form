d4c721becec39adf6be7354153dd2724f080717c
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-07 13:08:51 +0430

    Use the touchHandler function to handle errors

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -11,15 +11,16 @@ const SignUp = () => {
     });
     //------------------------------------------
     const [errors, setErrors] = useState({});
+    const [touched, setTouched] = useState({});
     //------------------------------------------
     useEffect ( () => {
         setErrors(validation(data))
-        console.log(errors)
+        // console.log(errors)
         },[data]
     )
 
     //------------------------------------------
-    const changeHandler =  event=> {
+    const changeHandler =  event => {
        if(event.target.name === 'isAccepted'){
             setData({...data, [event.target.name]: event.target.checked});
        }
@@ -28,6 +29,12 @@ const SignUp = () => {
        }   
        console.log(data)
     }
+    
+    //------------------------------------------
+    const focusHandler = event=> {
+        setTouched({...touched, [event.target.name]: true })
+    }
+
     //------------------------------------------
     return (
         <div>
@@ -36,32 +43,32 @@ const SignUp = () => {
                 <h2>SignUp</h2>
                 <div>
                     <label>Name</label>
-                    <input type="text" name='name' value={data.name} onChange = {changeHandler}/>
-                    {errors.name && <span>{errors.name}</span>}
+                    <input type="text" name='name' value={data.name} onChange = {changeHandler} onFocus={focusHandler}/>
+                    {errors.name && touched.name && <span>{errors.name}</span>}
                 </div>
                 
                 <div>
                     <label>Email</label>
-                    <input type="text" name='email' value={data.email} onChange = {changeHandler}/>
-                    {errors.email && <span>{errors.email}</span>}
+                    <input type="text" name='email' value={data.email} onChange = {changeHandler} onFocus={focusHandler}/>
+                    {errors.email && touched.name &&  <span>{errors.email}</span>}
                 </div>
 
                 <div>
                     <label>Password</label>
-                    <input type="password" name='password' value={data.password} onChange = {changeHandler}/>
-                    {errors.password && <span>{errors.password}</span>}
+                    <input type="password" name='password' value={data.password} onChange = {changeHandler} onFocus={focusHandler}/>
+                    {errors.password && touched.name &&  <span>{errors.password}</span>}
                 </div>
 
                 <div>
                     <label>Confirm Password</label>
-                    <input type="password" name='confirmPassword' value={data.confirmPassword} onChange = {changeHandler}/>
-                    {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+                    <input type="password" name='confirmPassword' value={data.confirmPassword} onChange = {changeHandler} onFocus={focusHandler}/>
+                    {errors.confirmPassword && touched.name &&  <span>{errors.confirmPassword}</span>}
                 </div>
 
                 <div>
                     <label>I accept terms of privacy policy</label>
-                    <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange = {changeHandler}/>
-                    {errors.isAccepted && <span>{errors.isAccepted}</span>}
+                    <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange = {changeHandler} onFocus={focusHandler}/>
+                    {errors.isAccepted && touched.name &&  <span>{errors.isAccepted}</span>}
                 </div>
 
                 <div>
