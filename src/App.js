14142c90f86e7209cf568eabb1f69e38550b00ae
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-11 12:52:47 +0430

    Add switch & route & redirect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
+import { Redirect, Route, Switch } from 'react-router-dom';
 const App = () => {
   return (
     <div>
-      <SignUp />
+      <Switch>
+        <Route path= "/signup" component={SignUp}/>
+        <Route path= "/login" component={Login}/>
+        <Redirect from='/' to="/signup" />
+      </Switch>
+      
     </div>
   );
 };
